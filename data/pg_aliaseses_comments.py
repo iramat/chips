256ea82fe2055f391de7d2353b90ef4df8c14618
HUET Thomas <TH282424@extra.cea.fr>
2026-02-27 09:47:33 +0100

    Deploy Doks site

--- data/pg_aliaseses_comments.py
+++ data/pg_aliaseses_comments.py
@@ -15,18 +15,25 @@ table_columns_comments=pd.read_csv("https://raw.githubusercontent.com/iramat/chi
 # Merge the dataframes
 # merged_df = table_aliases.merge(table_aliases, left_on="table_name", right_on="table_name", how="left")
 
-merged_df = pd.merge(table_aliases, table_columns_aliases, left_on=['table_name'], right_on=['table_name'], how="left")
+merged_df = pd.merge(table_columns_comments, table_columns_aliases, left_on=['table_name', 'column_name'], right_on=['table_name', 'column_name'], how="left")
 
-merged_df_all = pd.merge(merged_df, table_columns_comments, left_on=['table_name', 'column_name'], right_on=['table_name', 'column_name'], how="left")
+merged_df_all = pd.merge(merged_df, table_aliases, left_on=['table_name'], right_on=['table_name'], how="left")
 
-# merged_df_all.head(2)
+merged_df_all.head(2)
+
+# %%
 
 df = merged_df_all.drop(columns=['table_name', 'column_name', 'description'])
 df.rename(columns={'table_alias': 'table', 'column_alias': 'column'}, inplace=True)
 
 df.head(2)
 
-df.to_csv("C:\\Users\\TH282424\\Rprojects\\chips\\static\\data\\pg_aliaseses_comments.tsv", sep="\t", index=False)
+df = df[['table', 'column', 'comment']]
+
 
 # %%
+
+df.to_csv("C:\\Users\\TH282424\\Rprojects\\chips\\static\\data\\pg_aliaseses_comments.tsv", sep="\t", index=False)
+
+
 
